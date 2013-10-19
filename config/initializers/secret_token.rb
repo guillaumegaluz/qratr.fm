5d813bf8859ce2839d815454838d9a52699fa7e7
Guillaume Galuz <guillaume.galuz@gmail.com>
2013-10-18 18:25:19 -0700

    Create Rails app and bundle

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+QratrRails::Application.config.secret_key_base = '46c2722816b374574c846d79541c7f094286eef19f7eb38a7d525887bfafac73756ff110a1e68f71f824084f4ce2899a8530b0899eda9287761fe4fca17e2904'
