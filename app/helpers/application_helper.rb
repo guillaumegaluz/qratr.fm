80b1de5cf19c2a4d43b1a922aa642b2f9d957a58
Guillaume Galuz <guillaume.galuz@gmail.com>
2013-10-19 10:28:31 -0700

    Implement Prev and Next buttons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,22 @@
 module ApplicationHelper
+
+  def prev_playlist(playlist_id)
+    begin
+      prev_playlist = Playlist.find(playlist_id - 1)
+    rescue
+      nil
+    else
+      prev_playlist.url
+    end
+  end
+
+  def next_playlist(playlist_id)
+    begin
+      next_playlist = Playlist.find(playlist_id + 1)
+    rescue
+      nil
+    else
+      next_playlist.url
+    end
+  end
 end
--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,6 +1,10 @@
 class Playlist < ActiveRecord::Base
   has_many :tracks, -> { order(:position) }
 
+  def url
+    "/playlists/#{id}"
+  end
+
   def as_json_with_tracks
     json_hash = self.as_json
     json_hash['tracks'] = self.tracks.map(&:as_json)
