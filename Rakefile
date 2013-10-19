49ba491846b3328ae85a0e832b4c1179bf3477f5
Guillaume Galuz <guillaume.galuz@gmail.com>
2013-10-18 18:55:51 -0700

    Add seeds

--- Rakefile
+++ Rakefile
@@ -2,5 +2,10 @@
 # for example lib/tasks/capistrano.rake, and they will automatically be available to Rake.
 
 require File.expand_path('../config/application', __FILE__)
+require './db/seeds'
 
 QratrRails::Application.load_tasks
+
+task 'db:seed' do
+  Seeds.generate_data
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+class Seeds
+  def self.generate_data
+    Playlist.destroy_all
+    Track.destroy_all
+    self.generate_playlist
+    self.generate_tracks
+  end
+
+  def self.generate_playlist
+    @playlist = Playlist.create(:name => "A few songs to bring on a desert island")
+  end
+
+  def self.generate_tracks
+    TrackCreator.new("https://soundcloud.com/terekke/pianodub").build
+    TrackCreator.new("https://soundcloud.com/youngmarco/hnny-mys-young-marco-remix").build
+    TrackCreator.new("https://soundcloud.com/wake-up-records/wake-up-001-rising-sun-b1?in=wake-up-records/sets/wake-up-001-rising-sun-awake").build
+    TrackCreator.new("https://soundcloud.com/awesometapesfromafrica/shilela").build
+    TrackCreator.new("https://soundcloud.com/ghostly/fort-romeau-jetee").build
+    TrackCreator.new("https://soundcloud.com/analog-africa/florence-ayiero-orchestra").build
+    TrackCreator.new("https://soundcloud.com/thrilljockey/future-islands-before-the").build
+
+    Track.all.each { |t| t.update_attribute(:playlist_id, @playlist.id) }
+  end
+end
