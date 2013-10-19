1eebf0580138d39ccb9ab524b0f162f70008e7a5
Guillaume Galuz <guillaume.galuz@gmail.com>
2013-10-18 18:33:57 -0700

    Add Track and Playlist models

---
+++ app/models/playlist.rb
@@ -0,0 +1,9 @@
+class Playlist < ActiveRecord::Base
+  has_many :tracks, :order => "position"
+
+  def as_json_with_tracks
+    json_hash = self.as_json
+    json_hash['tracks'] = self.tracks.map(&:as_json)
+    json_hash
+  end
+end
