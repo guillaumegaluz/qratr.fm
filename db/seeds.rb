ff7ea8901ff56b74cdfa6fd1f0f13d5d07addb9c
Guillaume Galuz <guillaume.galuz@gmail.com>
2013-11-10 15:33:18 -0800

    Improve seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,59 @@
 class Seeds
+
+  @playlist_1_urls = [
+    "https://soundcloud.com/terekke/pianodub",
+    "https://soundcloud.com/youngmarco/hnny-mys-young-marco-remix",
+    "https://soundcloud.com/wake-up-records/wake-up-001-rising-sun-b1?in=wake-up-records/sets/wake-up-001-rising-sun-awake",
+    "https://soundcloud.com/awesometapesfromafrica/shilela",
+    "https://soundcloud.com/ghostly/fort-romeau-jetee",
+    "https://soundcloud.com/analog-africa/florence-ayiero-orchestra",
+    "https://soundcloud.com/thrilljockey/future-islands-before-the"
+  ]
+
+  @playlist_2_urls = [
+    "https://soundcloud.com/colonelgatito/ivan-smagghes-bbc-6mix",
+    "https://soundcloud.com/l-i-e-s/jahiliyya-fields-aeon-anon",
+    "https://soundcloud.com/oldich-dress-drink/chris-kontos-kennedy-party",
+    "https://soundcloud.com/dummymag/dummy-mix-186-fort-romeau",
+    "https://soundcloud.com/coopersaver/farawaymix",
+    "https://soundcloud.com/kim-brown/sets/somewhere-else-its-going-to-be",
+    "https://soundcloud.com/hessleaudio/joe-slope"
+  ]
+
+  @playlist_3_urls = [
+    "https://soundcloud.com/mexicansummer/why-are-you-crying?in=mexicansummer/sets/connan-mockasin-caramel",
+    "https://soundcloud.com/platform/prins-thomas-b2b-gerd-janson",
+    "https://soundcloud.com/justin-van-der-volgen/alexander-robotnick"
+  ]
+
   def self.generate_data
-    Playlist.destroy_all
-    Track.destroy_all
+    self.reset_database
     self.generate_playlist
     self.generate_tracks
   end
 
+  def self.reset_database
+    Playlist.destroy_all
+    Track.destroy_all
+  end
+
   def self.generate_playlist
-    @playlist = Playlist.create(:name => "A few songs to bring on a desert island")
+    @playlist_1 = Playlist.create(:name => "October '13")
+    @playlist_2 = Playlist.create(:name => "November '13")
+    @playlist_3 = Playlist.create(:name => "December '13")
   end
 
   def self.generate_tracks
-    TrackCreator.new("https://soundcloud.com/terekke/pianodub").build
-    TrackCreator.new("https://soundcloud.com/youngmarco/hnny-mys-young-marco-remix").build
-    TrackCreator.new("https://soundcloud.com/wake-up-records/wake-up-001-rising-sun-b1?in=wake-up-records/sets/wake-up-001-rising-sun-awake").build
-    TrackCreator.new("https://soundcloud.com/awesometapesfromafrica/shilela").build
-    TrackCreator.new("https://soundcloud.com/ghostly/fort-romeau-jetee").build
-    TrackCreator.new("https://soundcloud.com/analog-africa/florence-ayiero-orchestra").build
-    TrackCreator.new("https://soundcloud.com/thrilljockey/future-islands-before-the").build
+    @playlist_1_urls.each do |track_url|
+      @playlist_1.tracks << TrackCreator.new(track_url).build
+    end
 
-    Track.all.each { |t| t.update_attribute(:playlist_id, @playlist.id) }
+    @playlist_2_urls.each do |track_url|
+      @playlist_2.tracks << TrackCreator.new(track_url).build
+    end
+
+    @playlist_3_urls.each do |track_url|
+      @playlist_3.tracks << TrackCreator.new(track_url).build
+    end
   end
 end
