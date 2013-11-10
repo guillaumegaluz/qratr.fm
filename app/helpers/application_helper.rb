d18e371dda7159ff0f125d47b11f233b38d8f253
Guillaume Galuz <guillaume.galuz@gmail.com>
2013-11-10 15:33:34 -0800

    Add next/prev playlist names

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,7 @@
 module ApplicationHelper
 
-  def prev_playlist(playlist_id)
+  # Prev and Next playlists URL
+  def prev_playlist_url(playlist_id)
     begin
       prev_playlist = Playlist.find(playlist_id - 1)
     rescue
@@ -10,13 +11,34 @@ module ApplicationHelper
     end
   end
 
-  def next_playlist(playlist_id)
+  def next_playlist_url(playlist_id)
     begin
       next_playlist = Playlist.find(playlist_id + 1)
     rescue
       nil
     else
       next_playlist.url
+    end
+  end
+
+  # Prev and Next playlists names
+  def prev_playlist_name(playlist_id)
+    begin
+      prev_playlist = Playlist.find(playlist_id - 1)
+    rescue
+      nil
+    else
+      prev_playlist.name
+    end
+  end
+
+  def next_playlist_name(playlist_id)
+    begin
+      next_playlist = Playlist.find(playlist_id + 1)
+    rescue
+      nil
+    else
+      next_playlist.name
     end
   end
 end
