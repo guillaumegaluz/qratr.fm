d88017da63fec0f749e954df31e3ee3b61b8cabf
Guillaume Galuz <guillaume.galuz@gmail.com>
2013-11-11 19:47:43 -0800

    Implement TrackDecorator#mix? with threshold at 20min

--- app/decorators/track_decorator.rb
+++ app/decorators/track_decorator.rb
@@ -2,6 +2,7 @@ class TrackDecorator < Draper::Decorator
   delegate_all
 
   DEFAULT_TRACK_ARTWORK_URL = "http://www.whosampled.com/static/track_images_100/mr8231_201031_132914965488.jpg"
+  MIX_MINIMUM_TIME = 20 * 60 * 1000
 
   # -------------------------
   # Override track attributes
@@ -16,5 +17,9 @@ class TrackDecorator < Draper::Decorator
     nb_minutes = duration_in_seconds / 60
     nb_seconds = (duration_in_seconds % 60).floor
     return "#{nb_minutes}:#{nb_seconds}"
+  end
+
+  def mix?
+    object.duration > MIX_MINIMUM_TIME
   end
 end
