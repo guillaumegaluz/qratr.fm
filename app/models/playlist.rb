3789c53d10be22f3b595b1db1816b0d203c7f4b3
Guillaume Galuz <guillaume.galuz@gmail.com>
2013-11-15 17:31:12 -0700

    Add has_prev_playlist and has_next_playlist flags to Playlist#as_json

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -5,10 +5,21 @@ class Playlist < ActiveRecord::Base
     ApplicationHelper::Url::root_domain + "/playlists/#{id}"
   end
 
+  def has_prev_playlist
+    id != Playlist.first.id
+  end
+
+  def has_next_playlist
+    id != Playlist.last.id
+  end
+
   def as_json_with_tracks
-    json_hash = self.as_json
-    json_hash['url'] = self.url
-    json_hash['tracks'] = self.tracks.map(&:as_json)
-    json_hash
+    additional_data = {
+      url: url,
+      has_prev_playlist: has_prev_playlist,
+      has_next_playlist: has_next_playlist,
+      tracks: tracks.map(&:as_json)
+    }
+    self.as_json.merge(additional_data)
   end
 end
