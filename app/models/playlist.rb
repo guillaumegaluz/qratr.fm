6500d65e50525544c047b05815e5f9c372003958
Guillaume Galuz <guillaume.galuz@gmail.com>
2013-11-15 17:31:43 -0700

    Add some TODOs

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,18 +1,22 @@
 class Playlist < ActiveRecord::Base
   has_many :tracks, -> { order(:position) }
 
+  # TODO - Test this method
   def url
     ApplicationHelper::Url::root_domain + "/playlists/#{id}"
   end
 
+  # TODO - Test this method
   def has_prev_playlist
     id != Playlist.first.id
   end
 
+  # TODO - Test this method
   def has_next_playlist
     id != Playlist.last.id
   end
 
+  # TODO - Test this method
   def as_json_with_tracks
     additional_data = {
       url: url,
