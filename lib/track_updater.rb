7d0dd3d8ef7abd8fce45338629b49fb7b1875f2f
Guillaume Galuz <guillaume.galuz@gmail.com>
2013-11-30 18:37:35 -0800

    Fix issue with TrackUpdater refactoring

--- lib/track_updater.rb
+++ lib/track_updater.rb
@@ -2,14 +2,14 @@ require 'soundcloud'
 require 'soundcloud_api'
 
 class TrackUpdater
-  def self.update(track_id, attributes)
+  def self.update(track_id, properties)
     track_url = Track.find(track_id).permalink_url
     attributes = SoundCloudAPI.track_hash(track_url)
 
-    track.update_attribute('artwork_url', track_hash['artwork_url'] || track_hash["user"]["avatar_url"])  if attributes.include?('artwork_url')
+    Track.find(track_id).update_attribute('artwork_url', attributes[:artwork_url])  if properties.include?('artwork_url')
   end
 
-  def self.update_all(attributes)
-    Track.all.each { |track| update(track.id, attributes) }
+  def self.update_all(properties)
+    Track.all.each { |track| update(track.id, properties) }
   end
 end
