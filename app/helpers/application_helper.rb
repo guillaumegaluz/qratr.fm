add4d3244bce5008088516c1983f7fdb7cc28c09
Guillaume Galuz <guillaume.galuz@gmail.com>
2013-12-06 21:14:58 -0800

    Chaange root_domain to port 5000

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   class Url
     def self.root_domain
-      ENV["ROOT_DOMAIN"] || "http://localhost:3000"
+      ENV["ROOT_DOMAIN"] || "http://localhost:5000"
     end
   end
 end
