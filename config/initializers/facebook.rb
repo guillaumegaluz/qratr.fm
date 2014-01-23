cee33e5bf0277e3dcf99e9dffbee37c8c59321e0
Guillaume Galuz <guillaume.galuz@gmail.com>
2014-01-23 10:50:06 -0800

    Refactor facebook.rb

--- config/initializers/facebook.rb
+++ config/initializers/facebook.rb
@@ -1,10 +1,9 @@
-if Rails.env == 'development' || Rails.env == 'test'
-  Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :facebook, '1388716701376440', '76174072dc6964bee4e86edae1a7cded'
-  end
-else
-  # Production
+if Rails.env == 'production'
   Rails.application.config.middleware.use OmniAuth::Builder do
     provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+  end
+else
+  Rails.application.config.middleware.use OmniAuth::Builder do
+    provider :facebook, '1388716701376440', '76174072dc6964bee4e86edae1a7cded'
   end
 end
