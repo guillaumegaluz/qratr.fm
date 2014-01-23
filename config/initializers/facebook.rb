44a7d68bba63e6d93619b8eb582c71cc6fc6234b
Guillaume Galuz <guillaume.galuz@gmail.com>
2014-01-23 10:59:17 -0800

    Revert changes to facebook.rb

--- config/initializers/facebook.rb
+++ config/initializers/facebook.rb
@@ -1,13 +1,10 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+if Rails.env == 'development' || Rails.env == 'test'
+  Rails.application.config.middleware.use OmniAuth::Builder do
+    provider :facebook, '1388716701376440', '76174072dc6964bee4e86edae1a7cded'
+  end
+else
+  # Production
+  Rails.application.config.middleware.use OmniAuth::Builder do
+    provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_KEY']
+  end
 end
-
-# if Rails.env == 'production'
-#   Rails.application.config.middleware.use OmniAuth::Builder do
-#     provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
-#   end
-# else
-#   Rails.application.config.middleware.use OmniAuth::Builder do
-#     provider :facebook, '1388716701376440', '76174072dc6964bee4e86edae1a7cded'
-#   end
-# end
