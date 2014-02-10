82b3a2977221b564fcd2ae896ca56a64361b78ea
Guillaume Galuz <guillaume.galuz@gmail.com>
2014-02-09 16:27:03 -0800

    Write spec for TrackDecorator

---
+++ spec/decorators/track_decorator_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+require './app/models/track'
+require './app/decorators/track_decorator'
+
+describe TrackDecorator, :focus => true do
+  subject { TrackDecorator.new(track)}
+  let(:track) { FactoryGirl.build(:track) }
+
+  describe "#artwork_url" do
+    context "when the track doesn't have an artwork url" do
+      its(:artwork_url) { should eq("http://www.whosampled.com/static/track_images_100/mr8231_201031_132914965488.jpg") }
+    end
+
+    context "when the track has an artwork url" do
+      before(:each) { track.artwork_url = "hello" }
+      its(:artwork_url) { should eq("hello") }
+    end
+  end
+
+  describe "#duration" do
+    before(:each) { track.duration = 400000 }
+    its(:duration) { should eq("6:40") }
+  end
+end
