1e17b68359e6143cd20e061a160c8a962c6197b6
Guillaume Galuz <guillaume.galuz@gmail.com>
2014-02-09 16:29:29 -0800

    Remove TODOs

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,7 +1,6 @@
 class Playlist < ActiveRecord::Base
   has_many :tracks, -> { order(:duration) }
 
-  # TODO - Test this method
   def url
     ApplicationHelper::Url::root_domain + "/playlists/#{id}"
   end
@@ -14,12 +13,10 @@ class Playlist < ActiveRecord::Base
     Playlist.find(id + 1).name  if has_next_playlist
   end
 
-  # TODO - Test this method
   def has_prev_playlist
     id != Playlist.first.id
   end
 
-  # TODO - Test this method
   def has_next_playlist
     id != Playlist.last.id
   end
